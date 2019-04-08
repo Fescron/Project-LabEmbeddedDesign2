6934d7131987f751905e6043f5c9e20f6e9ca3df
Brecht Van Eeckhoudt <brecht.vaneeckhoudt@hotmail.com>
2019-04-08 20:17:14 +0200

    Update doxygen files with new sorting order

--- docs/interrupt_8h.js
+++ docs/interrupt_8h.js
@@ -1,6 +1,6 @@
 var interrupt_8h =
 [
+    [ "initGPIOwakeup", "interrupt_8h.html#a7e35708d61d26e9fe559a5545cdad810", null ],
     [ "BTN_getTriggered", "interrupt_8h.html#ab21b930d7d49ab29917b54408373414a", null ],
-    [ "BTN_setTriggered", "interrupt_8h.html#abb85f76e16c270396851fad940339a4b", null ],
-    [ "initGPIOwakeup", "interrupt_8h.html#a7e35708d61d26e9fe559a5545cdad810", null ]
+    [ "BTN_setTriggered", "interrupt_8h.html#abb85f76e16c270396851fad940339a4b", null ]
 ];
