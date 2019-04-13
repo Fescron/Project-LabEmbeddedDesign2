93a497b5eff0f315c51a43dd70e0be44ffd82118
Brecht Van Eeckhoudt <brecht.vaneeckhoudt@hotmail.com>
2019-04-13 16:48:20 +0200

    Updated doxygen documentation

---
+++ docs/adc_8h.js
@@ -0,0 +1,5 @@
+var adc_8h =
+[
+    [ "initADC", "adc_8h.html#af9fcfbd2b1ae50f7f6b26fda7476fcea", null ],
+    [ "readADC", "adc_8h.html#af543002bf30b32a9f33702edc642831d", null ]
+];
---
+++ docs/cable_8c.js
@@ -0,0 +1,4 @@
+var cable_8c =
+[
+    [ "checkCable", "cable_8c.html#af3994387a62811a825f973e1a09a533a", null ]
+];
