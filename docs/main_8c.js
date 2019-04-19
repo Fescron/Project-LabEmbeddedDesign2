30e2553f03a77b1f31178a17680bef30866f1e0f
Brecht Van Eeckhoudt <brecht.vaneeckhoudt@hotmail.com>
2019-04-19 21:50:36 +0200

    Updated doxygen documentation

--- docs/main_8c.js
+++ docs/main_8c.js
@@ -5,8 +5,8 @@ var main_8c =
     [ "mcu_states", "main_8c.html#a68d90e0eaaa236e9664bbfc691bbd4d4", [
       [ "INIT", "main_8c.html#a68d90e0eaaa236e9664bbfc691bbd4d4a0cb1b2c6a7db1f1084886c98909a3f36", null ],
       [ "MEASURE", "main_8c.html#a68d90e0eaaa236e9664bbfc691bbd4d4a10b4381a926db55ed18619513fb12a28", null ],
-      [ "SLEEP", "main_8c.html#a68d90e0eaaa236e9664bbfc691bbd4d4ad6137abebe4fdc59e2f0f2c84bdbe3fa", null ],
-      [ "WAKE_UP", "main_8c.html#a68d90e0eaaa236e9664bbfc691bbd4d4a02322edd4ce56f5e7e278a3b230062d1", null ]
+      [ "SEND", "main_8c.html#a68d90e0eaaa236e9664bbfc691bbd4d4a67be7a5f47004345047b32fff72a2230", null ],
+      [ "SLEEP", "main_8c.html#a68d90e0eaaa236e9664bbfc691bbd4d4ad6137abebe4fdc59e2f0f2c84bdbe3fa", null ]
     ] ],
     [ "checkInterrupts", "main_8c.html#ad16e16b91af17288b858dbfa6962c056", null ],
     [ "main", "main_8c.html#a840291bc02cba5474a4cb46a9b9566fe", null ],
