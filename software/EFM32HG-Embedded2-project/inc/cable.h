2ccb6949e0894aa9b9caf530eb93bca6f6ecedd9
Brecht Van Eeckhoudt <brecht.vaneeckhoudt@hotmail.com>
2019-05-08 00:56:39 +0200

    Added functionality to send measurements on cable breaking detection

--- software/EFM32HG-Embedded2-project/inc/cable.h
+++ software/EFM32HG-Embedded2-project/inc/cable.h
@@ -1,7 +1,7 @@
 /***************************************************************************//**
  * @file cable.h
  * @brief Cable checking functionality.
- * @version 1.5
+ * @version 2.0
  * @author Brecht Van Eeckhoudt
  ******************************************************************************/
 
@@ -11,8 +11,13 @@
 #define _CABLE_H_
 
 
+/* Includes necessary for this header file */
+#include <stdbool.h>   /* "bool", "true", "false" */
+#include "datatypes.h" /* Definitions of the custom data-types */
+
+
 /* Public prototype */
-void checkCable (void);
+bool checkCable (MeasurementData_t data);
 
 
 #endif /* _CABLE_H_ */
--- software/EFM32HG-Embedded2-project/src/cable.c
+++ software/EFM32HG-Embedded2-project/src/cable.c
@@ -1,14 +1,10 @@
 /***************************************************************************//**
  * @file cable.c
  * @brief Cable checking functionality.
- * @version 1.5
+ * @version 2.0
  * @author
  *   Matthias Alleman@n
  *   Modified by Brecht Van Eeckhoudt
- * @details
- *   Originally this functionality should've detected a cable breaking event using
- *   an **interrupt**, but due to the low water resistance this unfortunately wasn't
- *   possible to implement.
  *
  * ******************************************************************************
  *
@@ -20,13 +16,36 @@
  *   @li v1.3: Changed filename to `cable.c`
  *   @li v1.4: Fixed cable checking method.
  *   @li v1.5: Moved more functionality to this file.
+ *   @li v2.0: Added functionality to also send taken measurements when a cable break is
+ *             detected and updated version number.
  *
- *   @todo
- *     - Also send measurements on cable breaking?
+ * ******************************************************************************
+ *
+ * @todo
+ *   **Future improvements:**@n
+ *     - Use two parallel lines who normally aren't connected and use an interrupt
+ *       to detect if they broke and get connected through the water.
  *
  * ******************************************************************************
  *
  * @section License
+ *
+ *   **Copyright (C) 2019 - Brecht Van Eeckhoudt**
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the **GNU General Public License** as published by
+ *   the Free Software Foundation, either **version 3** of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   *A copy of the GNU General Public License can be found in the `LICENSE`
+ *   file along with this source code.*
+ *
+ *   @n
  *
  *   Some methods use code obtained from examples from [Silicon Labs' GitHub](https://github.com/SiliconLabs/peripheral_examples).
  *   These sections are licensed under the Silabs License Agreement. See the file
@@ -46,10 +65,13 @@
 #include "pin_mapping.h"   /* PORT and PIN definitions */
 #include "debug_dbprint.h" /* Enable or disable printing to UART for debugging */
 #include "lora_wrappers.h" /* LoRaWAN functionality */
+#include "datatypes.h"     /* Definitions of the custom data-types */
+
 
 /* Local variable */
 /** Keep the amount of times a message has been send to indicate the cable is broken (only send 4 messages in total) */
 uint8_t cableBrokenSendTimes = 0;
+
 
 /* Local prototype */
 static bool checkCable_internal (void);
@@ -58,8 +80,15 @@ static bool checkCable_internal (void);
 /**************************************************************************//**
  * @brief
  *   Method to check if the wire is broken.
+ *
+ * @param[in] data
+ *   The struct which contains the measurements to send using LoRaWAN.
+ *
+ * @return
+ *   @li `true` - Measurements have been send so the index has to be set back to zero.
+ *   @li `false` - No LoRaWAN message has been send.
  *****************************************************************************/
-void checkCable (void)
+bool checkCable (MeasurementData_t data)
 {
 	/* Check if the cable is broken */
 	if (!checkCable_internal())
@@ -74,11 +103,14 @@ void checkCable (void)
 
 			initLoRaWAN(); /* Initialize LoRaWAN functionality */
 
-			sendCableBroken(true); /* Send the LoRaWAN message TODO: also send measurement data here? */
+			sendCableBroken(true);  /* Send the LoRaWAN message */
+			sendMeasurements(data); /* Send the measurements */
 
 			disableLoRaWAN(); /* Disable RN2483 */
 
 			cableBrokenSendTimes++; /* Increment the counter */
+
+			return (true);
 		}
 		else
 		{
@@ -87,6 +119,7 @@ void checkCable (void)
 			dbcrit("Cable broken but no longer sending the data");
 #endif /* DEBUG_DBPRINT */
 
+			return (false);
 		}
 	}
 	else
@@ -96,6 +129,7 @@ void checkCable (void)
 		dbinfo("Cable still intact");
 #endif /* DEBUG_DBPRINT */
 
+		return (false);
 	}
 }
 
