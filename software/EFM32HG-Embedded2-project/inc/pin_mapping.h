2d918e606881ab8972d59e7becffb0b0c254d635
Brecht Van Eeckhoudt <brecht.vaneeckhoudt@hotmail.com>
2019-09-30 22:34:48 +0200

    Added license information

--- software/EFM32HG-Embedded2-project/inc/pin_mapping.h
+++ software/EFM32HG-Embedded2-project/inc/pin_mapping.h
@@ -15,6 +15,32 @@
  *   @li v1.4: Added IIC definitions.
  *   @li v2.0: Updated version number.
  *
+ * ******************************************************************************
+ *
+ * @section License
+ *
+ *   **Copyright (C) 2019 - Brecht Van Eeckhoudt**
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the **GNU General Public License** as published by
+ *   the Free Software Foundation, either **version 3** of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   *A copy of the GNU General Public License can be found in the `LICENSE`
+ *   file along with this source code.*
+ *
+ *   @n
+ *
+ *   Some methods use code obtained from examples from [Silicon Labs' GitHub](https://github.com/SiliconLabs/peripheral_examples).
+ *   These sections are licensed under the Silabs License Agreement. See the file
+ *   "Silabs_License_Agreement.txt" for details. Before using this software for
+ *   any purpose, you must agree to the terms of that agreement.
+ *
  ******************************************************************************/
 
 
